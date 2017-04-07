421180af980fbcaf9499784842ab606d03e48bfe
Max Kolyadin <makcyxa-k@yandex.ru>
2017-04-07 18:45:35 +0300

    style updates

--- resources/views/directives/review.blade.php
+++ resources/views/directives/review.blade.php
@@ -1,10 +1,12 @@
 <div>
-    @{{ review.comment }}
-    <b ng-class="{
-        'color-green': review.rating == 5,
-        'color-gold': review.rating == 4,
-        'color-red': review.rating <= 3
-    }">Оценка - @{{ review.rating }}.</b>
+    <div class="double-spacing">
+        @{{ review.comment }}
+        <span class='review-rating' ng-class="{
+            'color-green': review.rating == 5,
+            'color-gold': review.rating == 4,
+            'color-red': review.rating <= 3
+        }">Оценка - @{{ review.rating }}.</span>
+    </div>
     <div class="reviews-item-signature">
         <span ng-if='review.tutor'>Преподаватель: <a href="#">@{{ review.tutor.last_name }} @{{ review.tutor.first_name }} @{{ review.tutor.middle_name }}</a><br /></span>
         Подготовка: ЕГЭ по @{{ Subjects.dative[review.id_subject] }} в <academic year='review.year'></academic> учебном году<br />
