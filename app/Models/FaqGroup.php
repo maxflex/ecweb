46edf01815703e158491f268943aa0a23fc1c35a
Max Kolyadin <makcyxa-k@yandex.ru>
2017-04-14 14:15:32 +0300

    faq ordering

--- app/Models/FaqGroup.php
+++ app/Models/FaqGroup.php
@@ -8,7 +8,7 @@ use App\Models\Faq;
 class FaqGroup extends Model
 {
     public $fillable = ['faq'];
-    
+
     public function faq()
     {
         return $this->hasMany(Faq::class, 'group_id')->orderBy('position', 'asc');
@@ -16,7 +16,7 @@ class FaqGroup extends Model
 
     public static function getAll()
     {
-        $groups = self::with('faq')->get();
+        $groups = self::with('faq')->orderBy('position', 'asc')->get();
         $groups->add(new FaqGroup([
             'faq'   => Faq::whereNull('group_id')->orderBy('position', 'asc')->get()
         ]));
