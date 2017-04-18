50b47f3930ef14c871cafee6d8281f681ebafe29
Max Kolyadin <makcyxa-k@yandex.ru>
2017-04-18 18:45:24 +0300

    Prices update

--- app/Service/Price.php
+++ app/Service/Price.php
@@ -2,35 +2,54 @@
 
 namespace App\Service;
 use Illuminate\Support\Facades\Redis;
-use Cache;
 
 /**
  * Ценник
  */
 class Price
 {
-    /**
-     * $part    – [1..4] – какую часть взять
-     * $chunk   – кол-во элементов в одной части
-     * $step    – порог, после которого цена начинает снижаться
-     * $price   – начальная цена
-     * $price_step  – шаг уменьшения цены
-     */
-    public static function parse($part, $chunk, $step = 32, $price = 1700, $price_step = 100)
-    {
-        // генерируем ценники
-        $prices = [];
-        foreach(range(1, 4) as $i) {
-            foreach(range(1, $step) as $j) {
-                $prices[] = $price;
-            }
-            $price -= $price_step;
-        }
-        // вырезаем из ценников нужную часть
-        $sum = array_slice($prices, ($part - 1) * $chunk, $chunk);
+    // шаг уменьшения цены
+    const PRICE_STEP = 100;
 
-        // суммируем
-        return collect($sum)->sum();
-        // return number_format(collect($sum)->sum(), 0, '', ' ');
+    // класс => начальная стоимость
+    const PRICES = [
+        9  => 1600,
+        10 => 1600,
+        11 => 1700,
+    ];
+
+    // пороги, после которого цена начинает снижаться (ед. изм: кол-во занятий)
+    const STEPS = [64, 96, 128];
+
+    /**
+     * $grade   – [9..11] класс
+     * $part    – [1..4] какую часть взять
+     * $chunk   – кол-во элементов в одной части (сколько взять)
+     */
+    public static function parse($grade, $part, $chunk)
+    {
+        // начальная цена в зависимости от класса
+        $price = self::PRICES[$grade];
+
+        $start = ($part - 1) * $chunk + 1;
+
+        // устанавливаем начальную стоимость
+        foreach (self::STEPS as $step) {
+            if ($start > $step) {
+                $price -= self::PRICE_STEP;
+            }
+        }
+
+        $sum = 0;
+        foreach(range($start, ($start + $chunk - 1)) as $step) {
+            $sum += $price;
+            // \Log::info("Lesson $step: {$price} rub. \t({$sum} rub.)");
+            // уменьшаем ценник
+            if (in_array($step, self::STEPS)) {
+                $price -= self::PRICE_STEP;
+            }
+        }
+
+        return $sum;
     }
 }
