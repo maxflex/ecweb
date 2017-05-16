6665387344dfd8931d1abe492c65210825180f00
Shamshod <shamik1551@mail.ru>
2017-05-16 12:01:25 +0300

    image thumbs

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -6,16 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    public $timestamps = false;
-    const STORAGE_DIR = 'storage/images/';
+    public $timestamps  = false;
+    const  STORAGE_DIR  = 'storage/images/';
+    const  THUMB_URL    = 'resize/small/';
 
     public $appends = [
-        'url'
+        'url',
+        'thumb_url'
     ];
 
     public function getUrlAttribute()
     {
         return config('app.crm-url') . self::STORAGE_DIR . $this->filename;
+    }
+
+    public function getThumbUrlAttribute()
+    {
+        return config('app.crm-url') . static::THUMB_URL . $this->filename;
     }
 
     public static function parse($ids, $hide_link = false)
