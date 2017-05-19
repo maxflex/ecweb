175aa6f672a9eaac39e298dcc4ce1cf5270d263b
Max Kolyadin <makcyxa-k@yandex.ru>
2017-05-19 18:01:50 +0300

    GA

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -16636,13 +16636,6 @@ function dataLayerPush(object) {
     window.dataLayer = window.dataLayer || []
     window.dataLayer.push(object)
 }
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5P7NGRQ');</script>
-<!-- End Google Tag Manager -->
 /*
 Constructor for the tooltip
 @ param options an object containing: marker(required), content(required) and cssClass(a css class, optional)
