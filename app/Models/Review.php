3803bd554006d5e93829919ac1d123f6b0c36f20
Max Kolyadin <makcyxa-k@yandex.ru>
2017-07-31 14:24:49 +0300

    Reviews update

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use App\Scopes\ReviewScope;
 use Illuminate\Database\Eloquent\Model;
+use App\Service\Cacher;
+use Cache;
+use DB;
 
 class Review extends Model
 {
@@ -13,7 +16,7 @@ class Review extends Model
     /**
      * Отзывы учеников с фотографиями
      */
-    public static function getStudent($limit = 2, $min_score = null, $grade = null, $subject_id = null)
+    public static function getStudent($limit = 2, $min_score = null, $grade = null, $subject_id = null, $tutor = null)
     {
         $query = self::withStudent()->where('users.photo_extension', '<>', '')->take($limit)->inRandomOrder();
 
@@ -34,7 +37,21 @@ class Review extends Model
             $query->where('teacher_reviews.id_subject', '=', $subject_id);
         }
 
-        return $query->get();
+        $reviews = $query->get();
+
+        if ($tutor) {
+            foreach($reviews as &$review) {
+                $review->tutor = Cache::remember(cacheKey('tutor', $review->id_teacher), 60 * 24, function() use ($review) {
+                    $tutor = DB::connection('egerep')->table('tutors')->whereId($review->id_teacher)->select('id', 'first_name', 'last_name', 'middle_name', 'subjects')->first();
+                    $tutor->subjects_string_common = implode(', ', array_map(function($subject_id) {
+                        return Cacher::getSubjectName($subject_id, 'name');
+                    }, explode(',', $tutor->subjects)));
+                    return $tutor;
+                });
+            }
+        }
+
+        return $reviews;
     }
 
     public function scopeWithStudent($query)
