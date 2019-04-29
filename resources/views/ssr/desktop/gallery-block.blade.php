9d247cbc0b1af2b45534aad4512e760826d3773f
maxflex <>
2019-04-29 16:23:56 +0300

    Gallery block update

--- resources/views/ssr/desktop/gallery-block.blade.php
+++ resources/views/ssr/desktop/gallery-block.blade.php
@@ -1,19 +1,21 @@
-<div class='full-width-wrapper block'>
-    <div class='photos'>
-        @foreach ($items as $item)
-            <img src="{{ $item->url }}" />
-        @endforeach
-    </div>
-</div>
-<div>
-    <div class='photo-controls'>
-        <div class='photo-controls__left flex-items'>
-            <img src='/img/svg/arrow-pointing-left.svg' />
-            <a>назад</a>
+<div class='block'>
+    <div class='full-width-wrapper'>
+        <div class='photos'>
+            @foreach ($items as $item)
+                <img src="{{ $item->url }}" />
+            @endforeach
         </div>
-        <div class='photo-controls__right flex-items'>
-            <a>вперед</a>
-            <img src='/img/svg/arrow-pointing-left.svg' />
+    </div>
+    <div>
+        <div class='photo-controls'>
+            <div class='photo-controls__left flex-items'>
+                <img src='/img/svg/arrow-pointing-left.svg' />
+                <a>назад</a>
+            </div>
+            <div class='photo-controls__right flex-items'>
+                <a>вперед</a>
+                <img src='/img/svg/arrow-pointing-left.svg' />
+            </div>
         </div>
     </div>
 </div>
