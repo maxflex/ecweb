956f6d8e81d4d36561512d9d1d6674b1661169f1
maxflex <>
2019-05-16 16:35:44 +0300

    Style updates

--- resources/views/ssr/desktop/gallery-block.blade.php
+++ resources/views/ssr/desktop/gallery-block.blade.php
@@ -1,10 +1,9 @@
 <div class='full-width-wrapper'>
     <div class='block__gallery'>
         @foreach ($items as $index => $item)
-        {{-- <div style='width: 25vw;height: 25vw;margin: 0 10px;display: flex; align-items:center;justify-content:center;font-size: 300px; border: 3px solid black'>
-            {{ $index + 1}}
-        </div> --}}
-            <img src="{{ $item->url }}" />
+            <div>
+                <img src="{{ $item->url }}" />
+            </div>
         @endforeach
     </div>
 </div>
