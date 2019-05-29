089565ce90d588a6fb1657b7481526dad7d124f1
maxflex <>
2019-05-29 17:52:57 +0300

    new icons

--- resources/views/ssr/mobile/icons-block.blade.php
+++ resources/views/ssr/mobile/icons-block.blade.php
@@ -2,7 +2,7 @@
     <div class='block__icons'>
         @foreach($items as $item)
         <div>
-            <img src='/img/svg/{{ $item->icon }}' />
+            <img src='/img/icons-new/pros/{{ $item->icon }}' />
             <h1>{{ $item->title }}</h1>
             <div>{{ $item->desc }}</div>
         </div>
